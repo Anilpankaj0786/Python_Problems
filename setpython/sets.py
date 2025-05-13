57cd7f10d7b3e3a09630dfdf0a15c89e200be31c
Anil <anilpankaj40@gmail.com>
2025-05-13 19:53:07 +0530

    python set quest

---
+++ setpython/sets.py
@@ -0,0 +1,47 @@
+
+# Question1: Given a list of numbers, remove all duplicate values and return the unique elements.
+# a=[]
+# size=int(input("enter a size"))
+# for i in range(size):
+#     val=int(input("enter a val"))
+#     a.append(val)
+# print(a)
+# b=set(a)
+# print(b)
+
+# Question2: Given two sets, find the common elements between them.
+# a={1,2,3,4}
+# b={3,4,5,6}
+# a=a.intersection(b)
+# print(a)
+
+
+
+# Question3: Given two sets, find elements that are unique to each set.
+# a={1,2,3,4}
+# b={3,4,5,6}
+# print(a^b)
+
+# Question4: Given two sets, return elements that are only in the first set.
+# a={1,2,3,4}
+# b={3,4,5,6}
+# print(a-b)
+
+# Question4: Given two sets, return elements that are only in the second set.
+# a={1,2,3,4}
+# b={3,4,5,6}
+# print(b-a)
+
+# Question5: Given two sets, check if the first set is a subset of the second.
+# a={1,2,3,4}
+# b={1,2}
+# print(b.issubset(a))
+
+
+# Question6: Given multiple sets, return a single set that contains all unique elements.
+# a={1,2,3,4}
+# b={3,4,5,6}
+# c={5,6,7,8}
+# d=a.union(b)
+# e=d.union(c)
+# print(e)
